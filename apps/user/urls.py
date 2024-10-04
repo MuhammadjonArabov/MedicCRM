8a7ea64964fa3ce195a43e0e7af30b2cf12b00d3
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2024-10-04 15:20:35 +0500

    login

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('seller-create/', views.SellerCreateAPIView.as_view(), name='seller-create')
+    path('seller/seller-create/', views.SellerCreateAPIView.as_view(), name='seller-create'),
+    path('seller/seller-login/', views.SellerLoginAPIView.as_view(), name='seller-login'),
 
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,7 +1,12 @@
+from OpenSSL.rand import status
+from django.template.context_processors import request
+from django.utils.datetime_safe import datetime
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from apps.user.serializers import UserSellerCreateSerializers
 from rest_framework import generics
-from apps.user.models import Seller
+from apps.user.models import Seller, User
 
 
 class SellerCreateAPIView(generics.CreateAPIView):
@@ -9,3 +14,35 @@ class SellerCreateAPIView(generics.CreateAPIView):
 
     def get_queryset(self):
         return Seller.objects.none()
+
+
+class SellerLoginAPIView(APIView):
+
+    def post(self, request, *args, **kwargs):
+        phone = request.data.get('phone')
+        password = request.data.get('password')
+
+        if not phone or not password:
+            return Response({'phone': 'Phone va password maydonlari majbury'}, status=400)
+
+        user = User.objects.filter(phone=phone).first()
+        if not user:
+            raise ValidationError({"error": "Siz ro'yxatdan o'tmagansiz"}, code=400)
+
+        if not user.check_password(password):
+            return Response({'msg': 'Siz kiritgan parol xato iltimos qaytadan kiriting'}, status=400)
+
+        user.last_login = datetime.now()
+        user.save()
+
+        seller = Seller.objects.filter(user=user, status="active").first()
+        page_permissions = [page.name for page in seller.page_permissions.all()] if seller else []
+        role = "admin" if user.is_superuser else "seller"
+
+        data = {
+            'role': role,
+            'permissions': page_permissions,
+            'msg': "Xush kelibsiz",
+        }
+        data.update(user.tokens())
+        return Response(data, status=200)

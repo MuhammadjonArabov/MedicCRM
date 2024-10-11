fe41bb44c627c3d5a5fc3904a07fa55e63838492
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2024-10-11 18:03:34 +0500

    good

--- core/asgi.py
+++ core/asgi.py
@@ -17,11 +17,11 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 django.setup()
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            # Include your WebSocket routes here
-        )
-    ),
-})
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             # Include your WebSocket routes here
+#         )
+#     ),
+# })
